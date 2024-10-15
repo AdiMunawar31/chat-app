8514e17c984a9f014dcf77592a910c5ec69d1c04
adimunawar31 <munawar.adi31@gmail.com>
2024-10-15 16:30:56 +0700

    no auth now

--- src/global/apiClient.ts
+++ src/global/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "https://chat-room-api-tau.vercel.app/api/chat",
-  withCredentials: true,
+  // withCredentials: true,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
--- src/global/socketClient.ts
+++ src/global/socketClient.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 const socketClient: Socket = io("https://chat-room-api-tau.vercel.app", {
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default socketClient;
