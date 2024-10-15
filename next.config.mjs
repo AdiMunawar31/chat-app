3994eb23b8740f4704520d6ede00dd3bfd07ee59
adimunawar31 <munawar.adi31@gmail.com>
2024-10-15 16:37:49 +0700

    safsadjlfhjshd

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["ui-avatars.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/socket.io/:path*",
+        destination: "https://chat-room-api-tau.vercel.app/socket.io/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/global/socketClient.ts
+++ src/global/socketClient.ts
@@ -2,6 +2,7 @@ import { io, Socket } from "socket.io-client";
 
 const socketClient: Socket = io("https://chat-room-api-tau.vercel.app", {
   // withCredentials: true,
+  transports: ["polling"],
 });
 
 export default socketClient;
